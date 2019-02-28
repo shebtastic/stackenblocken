a783aae2775e95418c767e2331bbf28f0b517a6c
Marian Momper <m.momper@postuning.com>
2019-02-28 18:55:05 +0100

    added file reader

--- 2019_hashcode/src/main/java/_hashcode/App.java
+++ 2019_hashcode/src/main/java/_hashcode/App.java
@@ -3,6 +3,10 @@
  */
 package _hashcode;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
@@ -10,5 +14,16 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+//        fileReader(args[0]);
+    }
+
+    private void fileReader(String file) {
+//        File file = new File();
+//        BufferedReader br = new BufferedReader(new FileReader(file));
+//        String limits[] = br.readLine().split(" ");
+//        _rows = Integer.valueOf(limits[0]);
+//        _columns = Integer.valueOf(limits[1]);
+//        _minIngredient = Integer.valueOf(limits[2]);
+//        _maxCells = Integer.valueOf(limits[3]);
     }
 }
