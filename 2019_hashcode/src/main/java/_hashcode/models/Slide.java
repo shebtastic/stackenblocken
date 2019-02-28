2fcca5808ca5c5e32faed1504457197acfa877be
Sheb <shebtastic@googlemail.com>
2019-02-28 19:57:21 +0100

    added slides

---
+++ 2019_hashcode/src/main/java/_hashcode/models/Slide.java
@@ -0,0 +1,26 @@
+package _hashcode.models;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Slide {
+    public final List<Picture> PICTURES;
+
+    public Slide(VerticalPicture verticalPicture, VerticalPicture secondVerticalPicture) {
+        this.PICTURES = Arrays.asList(verticalPicture, secondVerticalPicture);
+    }
+
+    public Slide(HorizontalPicture horizontalPicture) {
+        this.PICTURES = Arrays.asList(horizontalPicture);
+    }
+
+    public List<String> tags() {
+        return PICTURES.size() == 2
+            ? PICTURES.get(0).combineTags(PICTURES.get(1))
+            : PICTURES.get(0).TAGS;
+    }
+
+    public int tagOverlap(Slide slide) {
+        return 0;
+    }
+}
