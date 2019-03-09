1dc20d7abdb9fd50c380ab4f6597de28c2cab12e
Marian Momper <m.momper@postuning.com>
2019-03-09 20:54:50 +0100

    more logging for inputs files

--- 2019_hashcode/src/main/java/_hashcode/Reader.java
+++ 2019_hashcode/src/main/java/_hashcode/Reader.java
@@ -3,15 +3,20 @@ package _hashcode;
 import _hashcode.models.HorizontalPicture;
 import _hashcode.models.Picture;
 import _hashcode.models.VerticalPicture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.*;
 
 public class Reader {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Reader.class);
+
     public static ArrayList<Picture> read(String path) {
         ArrayList<Picture> pictures = new ArrayList<>();
         File file = new File(path);
         Integer tagCounter = 0;
+        int tagsPerPictureSum = 0;
         HashMap<String, Integer> tagDic = new HashMap<>();
         BufferedReader br = null;
         try {
@@ -22,6 +27,7 @@ public class Reader {
                 String[] inputArray = inputString.split(" ", 3);
                 String orientation = inputArray[0];
                 String[] tags = inputArray[2].split(" ");
+                tagsPerPictureSum += tags.length;
                 Set<Integer> intTags = new HashSet<>();
                 for (String tag : tags) {
                     if (tagDic.containsKey(tag)) {
@@ -37,6 +43,9 @@ public class Reader {
                     pictures.add(new VerticalPicture(i, intTags));
                 }
             }
+
+            int tagsPerPicture = tagsPerPictureSum / pictureCount;
+            LOGGER.info("read " + path + " with " + pictureCount + " pictures and " + tagCounter + " tags and ~" + tagsPerPicture + " tags/picture");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
