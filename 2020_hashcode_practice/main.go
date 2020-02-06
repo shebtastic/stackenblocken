008f034cab4aa91144278728af106a7253c107ef
Sheb <michael@sheb.rocks>
2020-02-06 20:24:29 +0100

    practice setup

---
+++ 2020_hashcode_practice/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func readFile(r io.Reader) []int {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	numbers := []int{}
+	for scanner.Scan() {
+		number, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatalf("ermahgerd! %#v\n", err)
+		}
+
+		numbers = append(numbers, number)
+		log.Println(number)
+	}
+
+	return numbers
+}
+
+func main() {
+	rfile, err := os.Open("input/a_example.in")
+	if err != nil {
+		log.Fatalf("omgah! %#v\n", err)
+	}
+
+	log.Printf("%#v", readFile(rfile))
+
+	wfile, err := os.Open("output/a.out")
+	if err != nil {
+		log.Printf("omgah omgah! %#v\n", err)
+		wfile, err = os.Create("output/a.out")
+		if err != nil {
+			log.Panicf("still ded! %#v\n", err)
+		}
+		log.Printf("nvm.\n")
+	}
+
+	writer := bufio.NewWriter(wfile)
+	writer.WriteString("diz be output")
+	writer.Flush()
+}
